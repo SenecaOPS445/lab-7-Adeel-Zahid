3b123cc83ee76a42cf738beb1449b6e4a68b1b13
Adeel Zahid <azahid19@myseneca.ca>
2024-07-31 16:38:27 -0400

    lab7a-i completed and submitted

--- lab7b.py
+++ lab7b.py
@@ -30,6 +30,22 @@ def sum_times(t1, t2):
     
     return sum
 
+def change_time(time, seconds):
+    time.second += seconds
+    while time.second < 0:
+        time.second += 60
+        time.minute -= 1
+    while time.minute < 0:
+        time.minute += 60
+        time.hour -= 1
+    while time.second >= 60:
+        time.second -= 60
+        time.minute += 1
+    while time.minute >= 60:
+        time.minute -= 60
+        time.hour += 1
+    return None
+
 
 def valid_time(t):
     """check for the validity of the time object attributes:
@@ -39,3 +55,5 @@ def valid_time(t):
     if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
         return False
     return True
+
+
